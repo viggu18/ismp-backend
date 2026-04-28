ec039fcea9413f2c06b1c1f2c6cd198fc4e5cc68
Vignesh <nayakvignesh18@gmail.com>
2026-04-28 23:28:19 +0530

    Update: Removed dist

--- dist/common/middleware/async-handler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.asyncHandler = void 0;
-const asyncHandler = (handler) => {
-    return (req, res, next) => {
-        void handler(req, res, next).catch(next);
-    };
-};
-exports.asyncHandler = asyncHandler;
--- dist/modules/messages/message.schemas.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sendMessageSchema = void 0;
-const zod_1 = require("zod");
-exports.sendMessageSchema = zod_1.z.object({
-    content: zod_1.z.string().min(1).max(5000),
-    attachmentUrl: zod_1.z.string().url().optional().nullable(),
-});
